6d5190cc2534d539f7af5008b3848bdeaa864e3c
jiren <yujienjoyfever@yahoo.co.jp>
2022-10-20 09:56:59 +0900

    fix-dev

--- next.config.js
+++ next.config.js
@@ -6,6 +6,22 @@ const nextConfig = {
     domains: ['localhost'],
   },
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'query',
+            key: 'p',
+            value: '4'
+          },
+        ],
+        destination: '/posts/p1',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
