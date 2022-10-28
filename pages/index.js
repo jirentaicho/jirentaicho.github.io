55daa48db65e24220bb420333c38276d7368b8ec
jiren <yujienjoyfever@yahoo.co.jp>
2022-10-29 02:20:35 +0900

    fix-params

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,9 @@ import Pagination from '../components/Pagination';
 import { CommonValue } from '../core/ValueConst';
 import Sidebar from '../components/Sidebar';
 
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 // 実行時に処理される？
 // 例えば記事情報を取得する・・・？
 // 事前にfetchする処理
@@ -43,6 +46,77 @@ export const getStaticProps = () => {
 }
 
 export default function Home({posts,pages}) {
+
+  const router = useRouter();
+  const post_id = router.asPath;
+  // パスによってリダイレクトさせる
+  const params = post_id.slice(4,post_id.length);
+  useEffect(() => {
+    if(params === String(116)){
+      router.replace("/posts/p116/");
+    }
+    if(params === String(2691)){
+      router.replace("/posts/p2691/");
+    }
+    if(params === String(1460)){
+      router.replace("/posts/p1460/");
+    }
+    if(params === String(535)){
+      router.replace("/posts/p535/");
+    }
+    if(params === String(2727)){
+      router.replace("/posts/2727/");
+    }
+    if(params === String(1720)){
+      router.replace("/posts/p1720/");
+    }
+    if(params === String(511)){
+      router.replace("/posts/p511/");
+    }
+    if(params === String(64)){
+      router.replace("/posts/p64/");
+    }
+    if(params === String(1254)){
+      router.replace("/posts/p1254/");
+    }
+    if(params === String(679)){
+      router.replace("/posts/p679/");
+    }
+    if(params === String(2101)){
+      router.replace("/posts/p2101/");
+    }
+    if(params === String(2761)){
+      router.replace("/posts/p2761/");
+    }
+    if(params === String(3424)){
+      router.replace("/posts/p3424/");
+    }
+    if(params === String(904)){
+      router.replace("/posts/p904/");
+    }
+    if(params === String(3521)){
+      router.replace("/posts/p3521/");
+    }
+    if(params === String(2817)){
+      router.replace("/posts/p2817/");
+    }
+    if(params === String(665)){
+      router.replace("/posts/p665/");
+    }
+    if(params === String(3448)){
+      router.replace("/posts/p3448/");
+    }
+    if(params === String(986)){
+      router.replace("/posts/p986/");
+    }
+    if(params === String(3398)){
+      router.replace("/posts/p3398/");
+    }
+    if(params === String(1369)){
+      router.replace("/posts/p1369/");
+    }
+  },[]);
+
   return ( 
     <div className='my-8 grid grid-cols-1 md:grid-cols-3'>
       <div className='col-span-2'>
